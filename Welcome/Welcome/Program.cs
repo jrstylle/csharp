489d417e9bf3c4780c8305618f0c6f6a6c11cbdd
José Rodrigues <adm@lfbnet.com.br>
2025-02-27 21:51:23 -0300

    cria o primeiro programa em csharp

---
+++ Welcome/Welcome/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Welcome;
+
+class Program
+{
+    static void Main()
+    {   
+        Console.WriteLine("Digite seu nome: ");
+        var name = Console.ReadLine();
+
+        if (String.IsNullOrEmpty(name))
+        {
+            name = "Fulano";
+        }
+
+        Console.WriteLine($"Olá, {name}. Seja bem-vindo");
+
+    }
+}
