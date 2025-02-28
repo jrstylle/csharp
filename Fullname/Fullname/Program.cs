eca8aea2a19da1e491bebb4959bacfa716bd4416
José Rodrigues <adm@lfbnet.com.br>
2025-02-27 22:34:57 -0300

    cria programa que concatene um nome e um sobrenome inseridos pelo usuário e ao final exibe o nome completo.

---
+++ Fullname/Fullname/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+
+namespace Fullname;
+
+class Program
+{
+    static void Main()
+    {   
+        Console.WriteLine("Digite o seu primeiro nome: ");
+        string? firstName = Console.ReadLine();
+
+        Console.WriteLine("Digite o seu sobrenome: ");
+        string? lastName = Console.ReadLine();
+
+        if (String.IsNullOrWhiteSpace(firstName)) firstName = "Fulano";
+        if (String.IsNullOrWhiteSpace(lastName)) lastName = "de Tal";
+
+        string fullName = CreateFullName(firstName, lastName);
+
+        Console.WriteLine(fullName);
+
+
+    }
+
+    static string CreateFullName(string firstName, string lastName)
+    {
+        return $"Seu nome inteiro é: {firstName} {lastName}";
+    }
+}
