afa1c08ec6f44e936fbfa3ca7733e69d65b4dee3
José Rodrigues <adm@lfbnet.com.br>
2025-03-03 18:18:05 -0300

    cria um programa que o usuário digita uma ou mais palavras  e é exibido a quantidade de caracteres que a palavra inserida tem

---
+++ WordCharacterCounter/WordCharacterCounter/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace WordCharacterCounter;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma ou mais palavras: ");
+        string? userInput = Console.ReadLine();
+
+        if (String.IsNullOrWhiteSpace(userInput))
+        {
+            Console.WriteLine("Nenhuma palavra foi digitada");
+            return;
+        }
+
+        string[] words = userInput.Split(' ');
+
+        foreach (string word in words)
+        { 
+            Console.WriteLine($"A palavra '{word}' tem {word.Length} caracteres");
+        }
+    }
+}
